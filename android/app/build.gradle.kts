5239f975b6e0baf40cd919927bd44d3f629e3148
Rioche Patrick <patrick.rioche@gmail.com>
2026-09-03 10:50:23 +0200

    Bump Android app to 0.6.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         minSdk = 26
         targetSdk = 36
 
-        versionCode = 5
-        versionName = "0.5.0"
+        versionCode = 6
+        versionName = "0.6.0"
 
         buildConfigField(
             "String",
