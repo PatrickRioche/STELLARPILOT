81712f761c658c0f2390a43ac5c939fe23f9d681
Rioche Patrick <patrick.rioche@gmail.com>
2026-09-06 20:34:28 +0200

    release(android): bump device build to 0.6.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         minSdk = 26
         targetSdk = 36
 
-        versionCode = 6
-        versionName = "0.6.0"
+        versionCode = 7
+        versionName = "0.6.1"
 
         buildConfigField(
             "String",
