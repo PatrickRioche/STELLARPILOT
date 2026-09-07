e68dd82982a15d9f4adcea5fbf3c552cf80408b5
Rioche Patrick <patrick.rioche@gmail.com>
2026-09-07 22:01:56 +0200

    chore(android): bump app to v0.6.5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         minSdk = 26
         targetSdk = 36
 
-        versionCode = 10
-        versionName = "0.6.4"
+        versionCode = 11
+        versionName = "0.6.5"
 
         buildConfigField(
             "String",
