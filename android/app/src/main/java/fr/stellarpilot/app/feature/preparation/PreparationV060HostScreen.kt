fda74c2c8a662398eb2f5fda7ee8757c0e8aab48
Rioche Patrick <patrick.rioche@gmail.com>
2026-09-09 00:31:25 +0200

    fix(android): restore clean preparation workflow

--- android/app/src/main/java/fr/stellarpilot/app/feature/preparation/PreparationV060HostScreen.kt
+++ android/app/src/main/java/fr/stellarpilot/app/feature/preparation/PreparationV060HostScreen.kt
@@ -184,7 +184,7 @@ fun PreparationV060HostScreen(
                 .fillMaxWidth()
                 .weight(1f)
         ) {
-            AssistantModeScreen(
+            PreparationV060Screen(
                 onOpenSky = onOpenSky,
                 connectionViewModel = connectionViewModel
             )
