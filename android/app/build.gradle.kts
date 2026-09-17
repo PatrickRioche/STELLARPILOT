12c713718a2dae6de67ee072f0b59af8f81d8bff
Rioche Patrick <patrick.rioche@gmail.com>
2026-09-17 20:53:32 +0200

    chore(android): mark restored field build as v0.6.9-r4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         minSdk = 26
         targetSdk = 36
 
-        versionCode = 17
-        versionName = "0.6.9-r3"
+        versionCode = 18
+        versionName = "0.6.9-r4"
 
         buildConfigField(
             "String",
