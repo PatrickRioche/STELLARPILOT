78ed375ae6d0e50c9e714b02d8fd4d9dc9690e29
Rioche Patrick <patrick.rioche@gmail.com>
2026-09-26 10:40:27 +0200

    fix(r6): use ColumnScope weight without invalid import

--- android/app/src/main/java/fr/stellarpilot/app/feature/preparation/PreparationV060HostScreen.kt
+++ android/app/src/main/java/fr/stellarpilot/app/feature/preparation/PreparationV060HostScreen.kt
@@ -3,7 +3,6 @@ package fr.stellarpilot.app.feature.preparation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.weight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import fr.stellarpilot.app.feature.connection.ConnectionViewModel
