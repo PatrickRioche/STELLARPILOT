d0f7d3f8721603e490b05541fc6d6a696aa8c48a
Rioche Patrick <patrick.rioche@gmail.com>
2026-09-26 10:39:08 +0200

    feat(r6): expose daytime flats from Preparation tab

--- android/app/src/main/java/fr/stellarpilot/app/feature/preparation/PreparationV060HostScreen.kt
+++ android/app/src/main/java/fr/stellarpilot/app/feature/preparation/PreparationV060HostScreen.kt
@@ -1,6 +1,11 @@
 package fr.stellarpilot.app.feature.preparation
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.weight
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import fr.stellarpilot.app.feature.connection.ConnectionViewModel
 
 @Composable
@@ -8,8 +13,16 @@ fun PreparationV060HostScreen(
     onOpenSky: () -> Unit,
     connectionViewModel: ConnectionViewModel
 ) {
-    AssistantV069FourStepScreen(
-        onOpenSky = onOpenSky,
-        connectionViewModel = connectionViewModel
-    )
+    Column(modifier = Modifier.fillMaxSize()) {
+        FlatCalibrationPanel(
+            serverBaseUrl = connectionViewModel.uiState.serverBaseUrl
+        )
+
+        Box(modifier = Modifier.weight(1f)) {
+            AssistantV069FourStepScreen(
+                onOpenSky = onOpenSky,
+                connectionViewModel = connectionViewModel
+            )
+        }
+    }
 }
